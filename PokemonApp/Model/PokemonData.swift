da097710f829841a52db0ac9db6fe6c58e3bd835
Anup Saud <anupsaud10@gmail.com>
2024-08-10 22:19:15 -0400

    Add Kingfisher dependency, implement data fetching and parsing with background color logic, and extend Data for custom parsing

---
+++ PokemonApp/Model/PokemonData.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonData.swift
+//  PokemonApp
+//
+//  Created by Anup Saud on 2024-08-10.
+//
+
+import Foundation
+
+struct PokemonData:Codable,Identifiable {
+    let id: Int
+    let name: String
+    let imageUrl: String
+    let type: String
+    let weight: Int
+    let height: Int
+    let attack: Int
+    let defence: Int
+    let description: String
+}
