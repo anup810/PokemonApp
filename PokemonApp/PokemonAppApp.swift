787ff35c092f25f9c80d8d69784e266486488910
Anup Saud <anupsaud10@gmail.com>
2024-08-10 21:28:09 -0400

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Anup Saud on 2024-08-10.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
